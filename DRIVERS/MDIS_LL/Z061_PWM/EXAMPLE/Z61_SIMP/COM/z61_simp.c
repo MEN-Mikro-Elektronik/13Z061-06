4c14c6894009d112da4bb32015d65c212caf1cda
KonradJPE <ktomasik@jpembedded.eu>
2019-03-15 08:33:57 +0100

    Add proper license - fix for previous commit

--- DRIVERS/MDIS_LL/Z061_PWM/EXAMPLE/Z61_SIMP/COM/z61_simp.c
+++ DRIVERS/MDIS_LL/Z061_PWM/EXAMPLE/Z61_SIMP/COM/z61_simp.c
@@ -16,13 +16,26 @@
  *
  *     Required: libraries: mdis_api
  *     \switches (none)
- */
+ *
  *
  *---------------------------------------------------------------------------
  * Copyright (c) 2006-2019, MEN Mikro Elektronik GmbH
  ****************************************************************************/
 
-static const char RCSid[]="$Id: z61_simp.c,v 1.2 2009/03/02 15:52:39 MRoth Exp $";
+ /*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <string.h>
